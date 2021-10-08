208ea2ea13cb6e93fd4ea6aa28fb28122b02b6a4
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-08 12:01:58 +0530

    ENH: add buttons and peoples list

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  return <>
+  <main>
+    <section class="container">
+      <h3> 0 - birthday today</h3>
+      <List />
+      <button onClick={()=> console.log('click on clear button')}>clear all</button>
+    </section>
+  </main>
+  </>;
 }
 
 export default App;
