4cb4912c8cee3e8f90121b93deca78b39b12dd1e
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-08 12:38:48 +0530

    ENH: people list added clear button added

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
 
-  return <>
-  <main>
-    <section class="container">
-      <h3> 0 - birthday today</h3>
-      <List />
-      <button onClick={()=> console.log('click on clear button')}>clear all</button>
-    </section>
-  </main>
-  </>;
+  const peopleHandler = () => {
+    setPeople([]);
+  };
+  return (
+    <>
+      <main>
+        <section class="container">
+          <h3> {people.length} - birthday today</h3>
+          <List people={people} />
+          <button onClick={peopleHandler}>clear all</button>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
