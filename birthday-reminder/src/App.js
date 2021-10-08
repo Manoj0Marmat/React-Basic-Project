8e034ff2204ad7a3840ed6dda9c3cfdb17d976e3
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-08 20:37:32 +0530

    ENH: reformate code

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -14,15 +14,15 @@ function App() {
   useEffect(() => {
     if (people.length !== 0) {
       gsap.from(app, { duration: 3, y: -1280 });
+      gsap.to(app, { duration: 1, css: { visibility: "visible" } });
+      gsap.to(heading, { duration: 2, css: { visibility: "visible" } }, 3);
+      gsap.to(button, { duration: 3, css: { visibility: "visible" } }, 4);
     }
-    gsap.to(app, { duration: 1, css: { visibility: "visible" } });
-    gsap.to(heading, { duration: 2, css: { visibility: "visible" } }, 3);
-    gsap.to(button, { duration: 3, css: { visibility: "visible" } }, 4);
   });
   return (
     <>
       <main>
-        <section class="container" ref={(el) => (app = el)}>
+        <section className="container" ref={(el) => (app = el)}>
           <h3 ref={(el) => (heading = el)}>{people.length} - birthday today</h3>
           <List people={people} />
           <button onClick={peopleHandler} ref={(el) => (button = el)}>
