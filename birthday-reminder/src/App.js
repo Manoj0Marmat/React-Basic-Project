4d1a603eb1cd30ea57c9906ba3711898725977a7
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-08 17:50:03 +0530

    ENH: gsap animation added

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import gsap from "gsap";
 import data from "./data";
 import List from "./List";
 function App() {
+  let app = useRef(null);
+  let heading = useRef(null);
+  let button = useRef(null);
   const [people, setPeople] = useState(data);
 
   const peopleHandler = () => {
     setPeople([]);
   };
+  useEffect(() => {
+    if (people.length !== 0) {
+      gsap.from(app, { duration: 3, y: -1280 });
+    }
+    gsap.to(app, { duration: 1, css: { visibility: "visible" } });
+    gsap.to(heading, { duration: 2, css: { visibility: "visible" } }, 3);
+    gsap.to(button, { duration: 3, css: { visibility: "visible" } }, 4);
+  });
   return (
     <>
       <main>
-        <section class="container">
-          <h3> {people.length} - birthday today</h3>
+        <section class="container" ref={(el) => (app = el)}>
+          <h3 ref={(el) => (heading = el)}>{people.length} - birthday today</h3>
           <List people={people} />
-          <button onClick={peopleHandler}>clear all</button>
+          <button onClick={peopleHandler} ref={(el) => (button = el)}>
+            clear all
+          </button>
         </section>
       </main>
     </>
--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const List = ({ people }) => {
+  let images = useRef(null);
+  let tl = new gsap.timeline();
+  const animation = (el) => {
+    images = el;
+    gsap.to(images, { duration: 5, css: { visibility: "visible" } }, 3);
+    // console.log(lastImg);
+
+    tl.from(images, { duration: 2, x: 1280 }, 3);
+    // tl.from(firstImg && lastImg, { duration: 0.5, y: 1280 });
+  };
   return (
     <>
       {people.map((person) => {
         const { id, name, age, image } = person;
         return (
-          <article key={id} class="person">
+          <article key={id} className="person" ref={(el) => animation(el)}>
             <img src={image} alt={name} />
             <div>
               <h4>{name}</h4>
