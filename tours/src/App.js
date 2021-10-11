073aef698e098ba5770ca412ff2ed02daec35cf7
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-11 17:07:34 +0530

    ADD: new tours project

---
+++ tours/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  return (
+    <section className="container">
+      <h1>Tours Project Setup</h1>
+    </section>
+  );
+}
+
+export default App;
