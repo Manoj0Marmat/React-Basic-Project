36d1fbe6a65d8dd160cf54b1b322920c92b8f652
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-15 07:10:43 +0530

    ENH: fetching data

--- tours/src/App.js
+++ tours/src/App.js
@@ -5,8 +5,36 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    console.log(tours);
+  };
+  // const fetchTours = async () => {
+  //   setLoading(true);
+  //   fetch(url)
+  //     .then((resp) => {
+  //       if (resp.status >= 200 && resp.status <= 299) {
+  //         return resp.json();
+  //       } else {
+  //         setLoading(false);
+  //         throw new Error(resp.statusText);
+  //       }
+  //     })
+  //     .then((resp) => {
+  //       setLoading(false);
+  //       console.log(resp);
+  //     })
+  //     .catch((error) => console.log(error));
+  // };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (
