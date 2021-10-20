fe98f9e832f9a82797268c7573a055af0a5a00e8
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-20 22:33:28 +0530

    ENH: add question component

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,8 +1,46 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState, useRef, useEffect } from "react";
+import questions from "./data";
+import data from "./data";
+import SingleQuestion from "./Question";
+import gsap from "gsap";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  let bgColor = useRef(null);
+  let body = document.getElementsByTagName("body");
+  const colorsList = [
+    "linear-gradient(15deg, #ffafbd, #ffc3a0)",
+    "linear-gradient(30deg, #2193b0, #6dd5ed)",
+    "linear-gradient(45deg, #0250c5, #d43f8d)",
+    "#linear-gradient(60deg, #cc2b5e, #753a88)",
+    "linear-gradient(75deg, #42275a, #734b6d)",
+    "linear-gradient(90deg, #43cea2, #185a9d)",
+  ];
+  const animation = () => {
+    let number = Math.floor(Math.random() * colorsList.length);
+
+    gsap.from(body, { duration: 3, background: colorsList[number] });
+    gsap.to(bgColor, { duration: 3, background: colorsList[number] });
+  };
+
+  useEffect(() => {
+    window.setInterval(function () {
+      animation();
+    }, 1900);
+  });
+
+  return (
+    <main>
+      <div className="container" ref={(el) => (bgColor = el)}>
+        <h3>questions and answers about login </h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
