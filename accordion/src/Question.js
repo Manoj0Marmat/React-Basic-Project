b01a118d0aafac3207d577a544005f179a3fda9e
“Manoj0Marmat” <manoj.marmat@datachai.ai>
2021-10-20 22:34:58 +0530

    ENH: render questions and info

--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState, useEffect, useRef } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import gsap from "gsap";
+
+const Question = ({ title, info }) => {
+  let accordionInfo = useRef(null);
+
+  const [showinfo, setShowinfo] = useState(false);
+
+  useEffect(() => {
+    if (showinfo) {
+      gsap.from(accordionInfo, {
+        duration: 0.5,
+        height: 30,
+        y: -20,
+      });
+    }
+  });
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button
+          className="btn"
+          onClick={() => {
+            setShowinfo(!showinfo);
+          }}
+        >
+          {showinfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showinfo && <p ref={(el) => (accordionInfo = el)}>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
